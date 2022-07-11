8881ae58cb74f374e62ecde57e09bca8fd904134
unknown <ganitniduniyanikunj10@gmail.com>
2022-07-11 22:13:14 +0530

    Wait Demo,Iframe,ScreenShot,Alert and Popups,Automation Framework

---
+++ Session-2/Aboutusclass.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAutomation_1
+{
+    class Aboutusclass
+    {
+        private IWebDriver driver;
+
+        public Aboutusclass(IWebDriver drive) 
+        {
+        
+            this.driver = drive;
+            PageFactory.InitElements(driver, this);
+
+            driver.Navigate().GoToUrl("https://www.simplilearn.com/about-us");
+
+            //screenshot : optional
+            ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+            Screenshot screenshot = screenshotDriver.GetScreenshot();
+            screenshot.SaveAsFile("D:/Simpli Learn/Dot_net/Demo/aboutus.png", ScreenshotImageFormat.Png);
+            Console.WriteLine("Screenshot saved Successfully");
+
+
+        }
+
+
+    }
+}
---
+++ Session-2/AdvanceFramework.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+
+namespace SeleniumAutomation_1
+{
+    
+    class AdvanceFramework
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+            //initializing google chrome driver and opening url
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.simplilearn.com/";
+            driver.Manage().Window.Maximize();
+
+
+            //screenshot : optional
+            ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+            Screenshot screenshot = screenshotDriver.GetScreenshot();
+            screenshot.SaveAsFile("D:/Simpli Learn/Dot_net/Demo/home.png", ScreenshotImageFormat.Png);
+            Console.WriteLine("Screenshot saved Successfully");
+
+            Aboutusclass about = new Aboutusclass(driver);
+            PageFactory.InitElements(driver, this);
+        }
+    }
+}
