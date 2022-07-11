e0754f784275ee82ec95966e98b9431d7fd0728c
unknown <ganitniduniyanikunj10@gmail.com>
2022-07-11 18:46:14 +0530

    Wait Demo

---
+++ Session-2/WaitDemo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using System;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+namespace SeleniumAutomation_1
+{
+    [TestClass]
+    public class WaitDemo
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var driver = new ChromeDriver()) {
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("http://www.google.com");
+
+
+                //wait time
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until
+                    (d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+                Console.WriteLine("Document Loaded");
+            }
+
+        }
+    }
+}
