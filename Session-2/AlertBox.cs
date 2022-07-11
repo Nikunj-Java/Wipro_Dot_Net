2da8a705f74e47686a1aa6eaf19846bf84e51f96
unknown <ganitniduniyanikunj10@gmail.com>
2022-07-11 21:03:34 +0530

    Wait Demo,Iframe,ScreenShot,Alert and Popups

---
+++ Session-2/AlertBox.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace SeleniumAutomation_1
+{
+    [TestClass]
+    public class AlertBox
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+                var driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://nxtgenaiacademy.com/alertandpopup/");
+
+                //this step produce an alert on the screen
+                driver.FindElement(By.Name("alertbox")).Click();
+
+                //switching the control of 'driver' tom the alert from main window
+                IAlert alert = driver.SwitchTo().Alert();
+
+                //Accept() is used to accepet the alert
+                alert.Accept();
+                Console.WriteLine("alert accepted");
+
+               //get the text from alert box
+                
+
+                 
+
+             
+        }
+    }
+}
