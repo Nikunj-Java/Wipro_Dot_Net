ef4e7a5d50aee6e9c897ea59f6c347659451bdae
unknown <ganitniduniyanikunj10@gmail.com>
2022-07-15 17:19:05 +0530

    Console App

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World! from Thetips4you");
+        }
+    }
+}
---
+++ ConsoleApp/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World! from Thetips4you";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleApp.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
